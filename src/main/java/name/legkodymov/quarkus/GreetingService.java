37449ca28d9accbf7d295caab5520dcdaaeed5c6
Легкодымов Сергей Алексеевич <rutven@gmail.com>
2020-10-01 21:42:57 +0300

    Added a greeting service and tests

---
+++ src/main/java/name/legkodymov/quarkus/GreetingService.java
@@ -0,0 +1,10 @@
+package name.legkodymov.quarkus;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+}
--- src/test/java/name/legkodymov/quarkus/HelloResourceTest.java
+++ src/test/java/name/legkodymov/quarkus/HelloResourceTest.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.UUID;
+
 @QuarkusTest
 public class HelloResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+        given().when().get("/hello").then().statusCode(200).body(is("hello, world!"));
     }
 
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given().pathParam("name", uuid).when().get("/hello/greeting/{name}").then().statusCode(200)
+                .body(is("Hello " + uuid));
+    }
 }
